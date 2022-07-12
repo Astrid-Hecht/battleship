b2bc646eb7beb2f9727a05d49b560de7dda4cbf9
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-07-12 15:17:00 -0700

    Add initialize method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,3 +1,11 @@
 require './lib/ship.rb'
 
 class Cell
+  attr_reader :coordinates, :ship, :empty?, :fired_upon?
+  def initialize(coord)
+    @coordinates = coord
+    @ship = nil
+    @empty? = true
+    @fired_upon? = false
+  end
+end
