ed144a6659285043170852beef27f759e8c317a6
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-07-12 15:16:26 -0700

    Add initialize test

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,3 +1,15 @@
 require './lib/ship.rb'
 
 RSpec.describe Ship do
+  before :each do
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  it 'was created correctly' do
+    expect(cruiser).to be_an_instance_of(Ship)
+    expect(cruiser.name).to eq("Cruiser")
+    expect(cruiser.length).to eq(3)
+    expect(cruiser.health).to eq(3)
+    expect(cruiser.sunk?).to eq(false)
+  end
+end
