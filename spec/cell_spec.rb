13be54cd3e7c295e992ac1505e7c61409d05fe00
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-07-12 15:15:23 -0700

    Refactor

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,19 +1,18 @@
 require './lib/ship.rb'
 require './lib/cell.rb'
 
-RSpec.describe Cell
-    before :each do
-        coord = "B4"
-        @cell = Cell.new(coord)
-        @cruiser = Ship.new("Cruiser", 3)
-    end
+RSpec.describe Cell do
+  before :each do
+    coord = "B4"
+    @cell = Cell.new(coord)
+    @cruiser = Ship.new("Cruiser", 3)
+  end
 
-    it 'was created correctly' do
-        expect(@cell).to be_instance_of(Cell)
-        expect(@cell.coordinates).to eq(coord)
-        expect(@cell.ship).to eq(nil)
-        expect(@cell.empty?).to eq(true)
-        expect(@cell.fired_upon?).to eq(false)
-    end
+  it 'was created correctly' do
+    expect(@cell).to be_instance_of(Cell)
+    expect(@cell.coordinates).to eq(coord)
+    expect(@cell.ship).to eq(nil)
+    expect(@cell.empty?).to eq(true)
+    expect(@cell.fired_upon?).to eq(false)
+  end
 end
-        
